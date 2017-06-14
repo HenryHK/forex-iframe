432220933ea863fc93e049e5817bd4dc1f41e726
lhan <henryhan.hku@gmail.com>
2017-06-14 16:39:57 +1000

    change port config

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded());
 app.set('views', path.join(__dirname, "app", "views"));
 app.use("/", router);
 
-app.listen(8080, function() {
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
     console.log("The app is running");
 });
